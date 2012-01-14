224ffd40f994459aa29b4f3e5ba355cd29cac3bf
Sean Conner <spc@conman.org>
2012-01-14 18:16:38 -0500

    Rename client socket

--- client.lua
+++ client.lua
@@ -1,11 +1,14 @@
 #!/usr/bin/env lua
 
-net = require "org.conman.net"
+proc = require "org.conman.process"
+net  = require "org.conman.net"
+
+lname = string.format("/tmp/acl-request.%d",proc.PID)
 
 raddr = net.address("/tmp/acl")
-laddr = net.address("/tmp/blah")
+laddr = net.address(lname)
 sock  = net.socket(laddr.family,'udp')
 sock:bind(laddr)
 
 sock:write(raddr,"blah")
-os.remove("/tmp/blah")
+os.remove(lname)
