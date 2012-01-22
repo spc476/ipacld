6d19ae5b6158c8fefb40b9d7474e8a1eb1be4322
Sean Conner <spc@conman.org>
2012-01-22 01:48:27 -0500

    Lua API for the server side of the protocol.

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 
-CC = gcc -std=c99 -Wall -Wextra -pedantic -g
+CC     = gcc -std=c99 -Wall -Wextra -pedantic -g
+LUALIB = /usr/local/lib/lua/5.1
 
-all: readacl.so ipacl-client.o ipacl-server.o ipacltest
-
+all: readacl.so ipacl-client.o ipacl-server.o ipacltest ipacl_s.so
 
 ipacltest : ipacltest.o ipacl-client.o
 	$(CC) -o $@ ipacltest.o ipacl-client.o
 
+ipacl_s.so : ipacllua-s.c ipacl-server.c ipacl-proto.h ipacl-server.h
+	$(CC) -shared -fPIC -o $@ ipacllua-s.c ipacl-server.c
+	
 ipacl-server.o : ipacl-server.c ipacl-proto.h ipacl-server.h
 	$(CC) -c $<
 	
@@ -22,6 +25,16 @@ ipacl-codec.o : ipacl-codec.c ipacl-proto.h
 readacl.so : readacl.c
 	$(CC) -shared -fPIC -o $@ $<
 	
+install:
+	install -d $(LUALIB)/org
+	install -d $(LUALIB)/org/conman
+	install -d $(LUALIB)/org/conman/net
+	install ipacl_s.so $(LUALIB)/org/conman/net
+
+remove:
+	/bin/rm -rf $(LUALIB)/org/conman/net/ipacl_s.so
 
 clean:
 	/bin/rm -rf readacl.so *~ *.o ipacltest
+
+
---
+++ ipacllua-s.c
@@ -0,0 +1,236 @@
+
+#define _BSD_SOURCE
+#define _FORTIFY_SOURCE 0
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
+
+#include <syslog.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/poll.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include "ipacl-proto.h"
+#include "ipacl-server.h"
+
+#define IPACL_SERVER	"ipacl:server"
+#define NET_SOCK	"net:sock"
+#define NET_ADDR        "net:addr"
+
+/**********************************************************************/
+
+	/*----------------------------------------------------------
+	; the follow structures are defined as for the Lua module
+	; org.conman.net.  Not all fields are used here.
+	;-----------------------------------------------------------*/
+
+typedef union sockaddr_all
+{
+  struct sockaddr     sa;
+  struct sockaddr_in  sin;
+  struct sockaddr_in6 sin6;
+  struct sockaddr_un  ssun; /* not used */
+} sockaddr_all__t;
+
+typedef struct sock
+{
+  int fh;
+} sock__t;
+
+typedef struct ipaclsock
+{
+  int fh;
+} ipaclsock__t;
+
+/************************************************************************/
+
+static int	ipaclluas_open		(lua_State *const);
+
+static int	ipaclluas___tostring	(lua_State *const);
+static int	ipaclluas_read		(lua_State *const);
+static int	ipaclluas_send_err	(lua_State *const);
+static int	ipaclluas_send_fd	(lua_State *const);
+static int	ipaclluas_close		(lua_State *const);
+
+/************************************************************************/
+
+static const luaL_reg mipacls_reg[] =
+{
+  { "open"		, ipaclluas_open	} ,
+  { NULL		, NULL			}
+};
+
+static const luaL_reg mipacl_servermeta[] =
+{
+  { "__tostring"	, ipaclluas___tostring	} ,
+  { "__gc"		, ipaclluas_close	} ,
+  { "read"		, ipaclluas_read	} ,
+  { "send_err"		, ipaclluas_send_err	} ,
+  { "send_fd"		, ipaclluas_send_fd	} ,
+  { "close"		, ipaclluas_close	} ,
+  { NULL		, NULL			}
+};
+
+/************************************************************************/
+
+static int ipaclluas_open(lua_State *const L)
+{
+  ipaclsock__t *sock;
+  int           rc;
+  
+  sock = lua_newuserdata(L,sizeof(ipaclsock__t));
+  rc   = ipacls_open(&sock->fh);
+  
+  if (rc == 0)
+  {
+    luaL_getmetatable(L,IPACL_SERVER);
+    lua_setmetatable(L,-2);
+  }
+  else
+    lua_pushnil(L);
+  
+  lua_pushnil(L);
+  return 2;
+}
+
+/************************************************************************/
+
+static int ipaclluas___tostring(lua_State *const L)
+{
+  ipaclsock__t *sock;
+  
+  sock = luaL_checkudata(L,1,IPACL_SERVER);
+  lua_pushfstring(L,"IPACL-SERVER:%d",sock->fh);
+  return 1;
+}
+
+/************************************************************************/
+
+static int ipaclluas_read(lua_State *const L)
+{
+  ipaclsock__t    *sock;
+  struct ucred     cred;
+  sockaddr_all__t *remote;
+  sockaddr_all__t *addr;
+  unsigned int     proto;
+  int              rc;
+  
+  sock   = (ipaclsock__t *)luaL_checkudata(L,1,IPACL_SERVER);
+  remote = lua_newuserdata(L,sizeof(sockaddr_all__t));
+  lua_createtable(L,0,3);
+  addr   = lua_newuserdata(L,sizeof(sockaddr_all__t));
+  rc     = ipacls_read_request(sock->fh,&remote->ssun,&cred,&addr->sa,&proto);
+
+  if (rc != 0)
+  {
+    lua_pushnil(L);
+    lua_pushnil(L);
+    lua_pushnil(L);
+    lua_pushinteger(L,rc);
+    return 4;
+  }
+  
+  luaL_getmetatable(L,NET_ADDR);
+  lua_setmetatable(L,-4);
+  luaL_getmetatable(L,NET_ADDR);
+  lua_setmetatable(L,-2);
+  
+  lua_pushinteger(L,cred.pid);
+  lua_setfield(L,-3,"pid");
+  lua_pushinteger(L,cred.uid);
+  lua_setfield(L,-3,"uid");
+  lua_pushinteger(L,cred.gid);
+  lua_setfield(L,-3,"gid");
+  
+  lua_pushinteger(L,0);
+  return 4;
+}
+
+/************************************************************************/
+
+static int ipaclluas_send_err(lua_State *const L)
+{
+  ipaclsock__t    *sock;
+  sockaddr_all__t *remote;
+  int              err;
+  int              rc;
+  
+  sock   = luaL_checkudata(L,1,IPACL_SERVER);
+  remote = luaL_checkudata(L,2,NET_ADDR);
+  err    = luaL_checkinteger(L,3);
+  rc     = ipacls_send_err(sock->fh,&remote->ssun,err);
+  
+  lua_pushboolean(L,rc == 0);
+  lua_pushinteger(L,rc);
+  return 2;
+}
+
+/************************************************************************/
+
+static int ipaclluas_send_fd(lua_State *const L)
+{
+  ipaclsock__t    *sock;
+  sockaddr_all__t *remote;
+  int              fh;
+  int              rc;
+  
+  sock   = luaL_checkudata(L,1,IPACL_SERVER);
+  remote = luaL_checkudata(L,2,NET_ADDR);
+  
+  if (lua_isnumber(L,3))
+    fh = lua_tointeger(L,3);
+  else if (lua_isuserdata(L,3))
+  {
+    sock__t *s = luaL_checkudata(L,3,NET_SOCK);
+    fh = s->fh;  
+  }
+  else
+    return luaL_error(L,"wrong type of file descriptor");
+
+  rc = ipacls_send_fd(sock->fh,&remote->ssun,fh);
+
+  lua_pushboolean(L,rc == 0);
+  lua_pushinteger(L,rc);
+  return 2;
+}
+
+/************************************************************************/
+
+static int ipaclluas_close(lua_State *const L)
+{
+  ipaclsock__t *sock;
+  
+  sock = luaL_checkudata(L,1,IPACL_SERVER);
+  lua_pushinteger(L,ipacls_close(sock->fh));
+  return 1;
+}
+
+/************************************************************************/
+
+int luaopen_org_conman_net_ipacl_s(lua_State *const L)
+{
+  luaL_newmetatable(L,IPACL_SERVER);
+  luaL_register(L,NULL,mipacl_servermeta);
+  lua_pushvalue(L,-1);
+  lua_setfield(L,-2,"__index");
+  
+  luaL_register(L,"org.conman.net.ipacl_s",mipacls_reg);
+  return 1;
+}
+
+/************************************************************************/
+
